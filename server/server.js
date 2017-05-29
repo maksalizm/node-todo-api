12dbabc9799bcf09592cc82d8bfb98541b1b84fe
maksalizm <maksalizm@gmail.com>
2017-05-29 20:11:54 +0900

    Add POST /users/login create

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 require('./config/config');
 
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
+
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -113,6 +115,20 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+    res.send(body);
 });
 
 app.listen(port, () => {
